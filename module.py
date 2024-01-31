7ec7ccfff579ab37218dc21c158abf6e613aea83
Sukh Sodhi <sukh00422@gmail.com>
2024-01-30 18:52:28 -0800

    First 3 done

---
+++ module.py
@@ -0,0 +1,11 @@
+import math
+
+def square_foot(l, w):
+    area = l * w
+    return area
+
+
+def circle_circumference(radius):
+    circumference = 2 * math.pi * radius
+    return circumference
+    
